d52fe5f5e0ec786b7ab44852d2ffa1229d243f74
gouf <innocent.zero@gmail.com>
2014-12-23 21:11:17 +0900

    Auto rewrite of coding style
    rubocop -a

--- lib/guard/rails_best_practices.rb
+++ lib/guard/rails_best_practices.rb
@@ -1,20 +1,19 @@
 require 'guard'
 require 'guard/compat/plugin'
 
-
-require File.join(File.dirname(__FILE__), "rails_best_practices/version")
+require File.join(File.dirname(__FILE__), 'rails_best_practices/version')
 
 module Guard
   class RailsBestPractices < Plugin
     autoload :Notifier, 'guard/rails_best_practices/notifier'
 
     def initialize(options = {})
-      rbp_opts = {  :vendor         => true,
-                    :spec           => true,
-                    :test           => true,
-                    :features       => true
+      rbp_opts = {  vendor: true,
+                    spec: true,
+                    test: true,
+                    features: true
                   }
-      guard_opts = { :run_at_start   => true }
+      guard_opts = { run_at_start: true }
       options = rbp_opts.merge options
       options = guard_opts.merge options
 
@@ -37,38 +36,38 @@ module Guard
       run_bestpractices
     end
 
-    def run_on_change(paths)
+    def run_on_change(_paths)
       run_bestpractices
     end
 
-    def run_on_deletion(paths)
+    def run_on_deletion(_paths)
     end
 
     def notify?
       !!options[:notify]
     end
 
-  private
+    private
+
     def run_bestpractices
       started_at = Time.now
 
       run_options = options.select { |key, value| value && ![:run_at_start, :notify].include?(key) }.keys.map do |opt|
-        if [:format, :exclude, :only].include?(opt) 
-          [ opt.to_s, options[opt] ].join(' ')
+        if [:format, :exclude, :only].include?(opt)
+          [opt.to_s, options[opt]].join(' ')
         else
-          opt.to_s.gsub('_','-')
+          opt.to_s.gsub('_', '-')
         end
       end
 
       cmd = (['rails_best_practices'] + run_options).join(' --')
 
-      UI.info "Running Rails Best Practices checklist with command\n=> #{cmd}\n", :reset => true
+      UI.info "Running Rails Best Practices checklist with command\n=> #{cmd}\n", reset: true
 
       @result = system(cmd)
 
-      Notifier::notify( @result, Time.now - started_at ) if notify?
+      Notifier.notify(@result, Time.now - started_at) if notify?
       @result
     end
-
   end
 end
